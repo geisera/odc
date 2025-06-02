4b0ef0fc7ad7cf5f038f835ff201e7606dc1ffaa
Tony <geisera@gmail.com>
2025-06-02 08:28:07 -0400

    universal nav bar in main.js

--- main.js
+++ main.js
@@ -2,3 +2,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('year').textContent = new Date().getFullYear();
 });
+
+document.getElementById('nav').innerHTML = `
+    <nav>
+      <div>
+          <h2>Orbital Defense Corps</h2>
+          <h3><em>The Journal of Military Science Fiction</em></h3>
+      </div>
+      <div>
+          <a href="#articles">Articles</a>
+          <a href="#signup">Subscribe</a>
+          <a href="/map.html">Map</a>
+      </div>
+    </nav>
+`;
