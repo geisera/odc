d5c2837b5966a6a4639ce227f482a3169a48f634
Tony <geisera@gmail.com>
2025-06-05 12:16:22 -0500

    experiments...

--- src/test.js
+++ src/test.js
@@ -1,4 +1,8 @@
-window.addEventListener("DOMContentLoaded", (event) => {
+ console.log('test-js : EXECUTED!');
+  let node = document.getElementById('test-heading');
+  node.innerHTML = 'INTEL REPORTS';
+  
+  window.addEventListener("DOMContentLoaded", (event) => {
   console.log('test-js : EXECUTED!');
   let node = document.getElementById('test-heading');
   node.innerHTML = 'INTEL REPORTS';
