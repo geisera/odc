4c887a7e4f940be9d9852b152dc2ea69556a5bde
Tony <geisera@gmail.com>
2025-06-05 12:20:00 -0500

    experiments...

--- src/test.js
+++ src/test.js
@@ -2,9 +2,9 @@
   let node = document.getElementById('test-heading');
   node.innerHTML = 'INTEL REPORTS';
   
-  window.addEventListener("DOMContentLoaded", (event) => {
-  console.log('test-js : EXECUTED!');
-  let node = document.getElementById('test-heading');
-  node.innerHTML = 'INTEL REPORTS';
+  window.addEventListener("readyStateChange", (event) => {
+    console.log('test-js : EXECUTED!');
+    let node = document.getElementById('test-heading');
+    node.innerHTML = 'INTEL REPORTS';
 
 });
