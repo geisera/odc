b389df1e102343e3e909f32335d6278addf0976f
Tony <geisera@gmail.com>
2025-06-05 11:11:57 -0500

    experiments...

--- utils/router.js
+++ utils/router.js
@@ -1,10 +1,9 @@
 console.log('URL router called!');
-
 document.addEventListener('click', (event) => {
     const { target } = event;
-    if (!target.matches('nav a')) {
+    if ( !target.matches('nav a') ) {
         return;
-    }
+    };
     event.preventDefault();
     route(event);
 });
@@ -14,46 +13,27 @@ const routes = {
     "/": { template: "/templates/index.html", title: "", description: "" },
     "/about": { template: "/templates/about.html", title: "", description: "" },
     "/contact": { template: "/templates/contact.html", title: "", description: "" }
-};
+}
 
 const route = (event) => {
+    event = event;
     event.preventDefault();
-    window.history.pushState({}, "", event.target.href);
+    window.history.pushState( {}, "", event.target.href );
     locationHandler();
-};
+}
 
 const locationHandler = async () => {
-    let location = window.location.pathname;
-    if (location.length === 0) location = "/";
+    const location = window.location.pathname;
+    if ( location.length == 0) {
+        location = '/';
+    }
 
-    const route = routes[location] || routes[404];
-    const html = await fetch(route.template).then((res) => res.text());
+    const route = routes[ location ] || routes[404];
+    const html = await fetch( route.template ).then( (response) => 
+    response.text() );
 
-    injectHTMLWithScripts('content', html);
+    document.getElementById('content').innerHTML = html;
 };
-
-function injectHTMLWithScripts(containerId, html) {
-    const container = document.getElementById(containerId);
-    container.innerHTML = '';
-
-    const temp = document.createElement('div');
-    temp.innerHTML = html;
-
-    [...temp.childNodes].forEach((node) => {
-        if (node.tagName === 'SCRIPT') {
-            const newScript = document.createElement('script');
-            if (node.src) {
-                newScript.src = node.src;
-            } else {
-                newScript.textContent = node.textContent;
-            }
-            newScript.type = node.type || 'text/javascript';
-            document.body.appendChild(newScript);
-        } else {
-            container.appendChild(node);
-        }
-    });
-}
 
 window.onpopstate = locationHandler;
 window.route = route;
