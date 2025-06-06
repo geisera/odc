c7422ab123ffcd545a13a7763ddab68a5a944d10
Tony <geisera@gmail.com>
2025-06-06 13:44:40 -0500

    implemented basic templating system

--- src/test.js
+++
@@ -1,6 +0,0 @@
-// src/test.js
-export function init() {
-  console.log("test.js :init()");
-  const node = document.getElementById("test-heading");
-  if (node) node.innerHTML = "INTEL REPORTS";
-}
