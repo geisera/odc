4a881c9d8b680a200e72bd1ad90f06b4a31bf0e5
Tony <geisera@gmail.com>
2025-06-10 09:16:53 -0500

    messing with structure

--- js/routes.js
+++ js/routes.js
@@ -4,7 +4,7 @@ export const routes = {
     data: {
       title: "HOME",
       heading: "Home",
-      content: "<b><em>Welcome</em></b> to our vanilla-JS boilerplate app! This is a simple single-page app built with plain HTML, CSS, and JavaScript. No frameworks.",
+      content: "Under the blazing light of Sol, we stand as humanity’s shield—unyielding, unwavering, unbeatable.",
     },
   },
   "/about": {
