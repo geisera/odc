63cbce51457a14c22ce57f39d0659149a354c96a
Tony <geisera@gmail.com>
2025-06-10 18:52:34 -0500

    amended propaganda

--- components/test.js
+++ components/test.js
@@ -1,5 +1,5 @@
 export const Test = `
-    <div>
+    <div class="container">
         <p><strong>Join the Sol Defense Corps</strong></p>
         <p>Heroes Wanted</p>
         <p>Brave volunteers sought for daring missions beyond Earth’s horizon. Facing uncertainty, risk, and unimaginable discoveries. Expect hardships, long voyages, and encounters that challenge the spirit and test resolve. Safe return uncertain.</p>
