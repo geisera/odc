963a4fc25313c1b18f7fee5c817202acbf4c1679
Tony <geisera@gmail.com>
2025-06-10 19:11:42 -0500

    amended propaganda style

--- components/Propaganda.js
+++ components/Propaganda.js
@@ -1,6 +1,6 @@
 export const Propaganda = `
     <div class="container">
-        <p><bold>Join the Sol Defense Corps</bold></p>
+        <p><b>Join the Sol Defense Corps</b></p>
         <p>Heroes Wanted</p>
         <p>Brave volunteers sought for daring missions beyond Earth’s horizon. Facing uncertainty, risk, and unimaginable discoveries. Expect hardships, long voyages, and encounters that challenge the spirit and test resolve.</p>
         <p>Rewards include honor, the camaraderie of fellow pioneers, and the everlasting gratitude of humanity.</p>
